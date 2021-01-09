27fbfaf8b4d8d4880153b121326d648d86342022
killian <killian.debuitleir@gmail.com>
2021-01-09 18:26:51 +0100

    test

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 // Create Schema
 const UserSchema = new Schema({
   name: {
@@ -17,6 +18,10 @@ const UserSchema = new Schema({
   date: {
     type: Date,
     default: Date.now,
+  },
+  data: {
+    type: String,
+    required: true,
   },
 });
 
